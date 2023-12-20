46bb5081978d2948873b603151d5fdb60aa608ae
⚘'Ivan'⚘ <78805030+Evanechecssss@users.noreply.github.com>
2023-12-20 15:31:29 +0500

    0.1

--- StableDiffusionProxy.py
+++ StableDiffusionProxy.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import aiohttp
 
@@ -6,17 +7,17 @@ class StableDiffusionProxy:
     header = {"Content-Type": "application/json"}
     payload = lambda self, prompt, sd_key: json.dumps({
         "key": sd_key,
-        "model_id": "midjourney",
         "prompt": f"Generate a prompt for a close-up profile portrait of a (({prompt})), focusing on the facial features and eyes. The image should be large enough to clearly show the details of the face and should not include the body",
         "negative_prompt": "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime",
         "width": "512",
         "height": "512",
         "samples": "1",
-        "num_inference_steps": "30",
+        "num_inference_steps": "20",
+        "safety_checker": "no",
+        "enhance_prompt": "yes",
         "seed": None,
         "guidance_scale": 7.5,
-        "safety_checker": "yes",
-        "multi_lingual": "yes",
+        "multi_lingual": "no",
         "panorama": "no",
         "self_attention": "no",
         "upscale": "no",
@@ -30,9 +31,10 @@ class StableDiffusionProxy:
 
     async def text_to_image(self, prompt):
         async with aiohttp.ClientSession() as session:
-            res = await session.post(url="https://stablediffusionapi.com/api/v4/dreambooth",
-                                     data=self.payload(prompt[1:], self.sd_key),
+            res = await session.post(url="https://stablediffusionapi.com/api/v3/text2img",
+                                     data=self.payload(prompt, self.sd_key),
                                      headers=self.header)
+            await asyncio.sleep(2)
             rg = await res.json()
             if "future_links" in rg:
                 return rg["future_links"][0]
--- clio_tg_bot.py
+++ clio_tg_bot.py
@@ -1,11 +1,14 @@
 from typing import Final
 
+import numpy as np
+import tensorflow
 from aiogram import executor
 from aiogram.types import Message, ContentType
 from aiogram.dispatcher.filters import Command
 from magic_filter import F
+from aiogram import types
 from tqdm import keras
-
+import io
 from cliogan.model_service import load_image_url, image_to_image, load_model
 from lexicon import lex, lex_format, answer_format
 from modules_import import *
@@ -21,6 +24,11 @@ class ClioTelegramBotHandler:
         self.dp = config.dp
         self.run()
 
+        self.model0 = load_model(0)
+        self.model1 = load_model(1)
+        self.model2 = load_model(2)
+        self.model3 = load_model(3)
+        self.models = [self.model0, self.model1, self.model2, self.model3]
         executor.start_polling(self.dp, skip_updates=False)
 
     def run(self):
@@ -42,7 +50,11 @@ class ClioTelegramBotHandler:
                     await self.bot.send_photo(message.chat.id, photo=url, caption=lex['source_image_notification'])
                     src_image = load_image_url(url, remove_bg=True)
                     for i in range(4):
-                        to_send = keras.utils.array_to_img(image_to_image(load_model(i), src_image)[0, :, :, :])
+                        to_send = tensorflow.keras.utils.array_to_img(image_to_image(self.models[i], src_image)[0, :, :, :])
+                        image_bytes = io.BytesIO()
+                        to_send.save(image_bytes, format="JPEG")
+                        image_bytes.seek(0)
+                        to_send = types.InputFile(image_bytes)
                         await self.bot.send_photo(message.chat.id, photo=to_send,
                                             caption=lex['end_gen_notification'] if i == 3 else lex_format('gen_notification', i))
                 else:
